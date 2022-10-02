8d3fcf3adc6c4ba0d1d7c0de1f9a31fc25090d84
Shahar6 <shaharb4r@gmail.com>
2022-10-02 17:58:10 +0300

    increase the limit of whatdrops from 3 to 10

--- src/main/java/client/command/commands/gm1/WhatDropsFromCommand.java
+++ src/main/java/client/command/commands/gm1/WhatDropsFromCommand.java
@@ -48,7 +48,7 @@ public class WhatDropsFromCommand extends Command {
         }
         String monsterName = player.getLastCommandMessage();
         String output = "";
-        int limit = 3;
+        int limit = 10;
         Iterator<Pair<Integer, String>> listIterator = MonsterInformationProvider.getMobsIDsFromName(monsterName).iterator();
         for (int i = 0; i < limit; i++) {
             if (listIterator.hasNext()) {

ced7b7b1ee27679f4fc1d071c695e8348393264d
Shahar6 <shaharb4r@gmail.com>
2022-10-08 23:53:19 +0300

    Buyback will only be available with the trade button

--- src/main/java/client/command/commands/gm0/BuyBackCommand.java
+++ src/main/java/client/command/commands/gm0/BuyBackCommand.java
@@ -25,24 +25,24 @@ package client.command.commands.gm0;
 
 import client.Client;
 import client.command.Command;
-import client.processor.action.BuybackProcessor;
+//import client.processor.action.BuybackProcessor;
 
 public class BuyBackCommand extends Command {
     {
-        setDescription("Revive yourself after a death.");
+        setDescription("Revive yourself after a death, use the trade button");
     }
 
     @Override
     public void execute(Client c, String[] params) {
-        if (params.length < 1) {
+        /*if (params.length < 1) {
             c.getPlayer().yellowMessage("Syntax: @buyback <info|now>");
             return;
         }
 
         if (params[0].contentEquals("now")) {
             BuybackProcessor.processBuyback(c);
-        } else {
+        } else {*/
             c.getPlayer().showBuybackInfo();
-        }
+       //}
     }
 }

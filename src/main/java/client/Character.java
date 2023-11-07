b95009b8fafef44c52dcf519099459891bcd19c1
leevccc <94272011+leevccc@users.noreply.github.com>
2023-11-07 20:31:26 +0800

    level up protect
    to prevent player level up 2 or more at one time

--- src/main/java/client/Character.java
+++ src/main/java/client/Character.java
@@ -3102,6 +3102,7 @@ public class Character extends AbstractCharacterObject {
                     updateSingleStat(Stat.EXP, 0);
                     break;
                 }
+                if (YamlConfig.config.server.USE_LV_UP_PROTECT) break;
             }
 
             if (leftover > 0) {
